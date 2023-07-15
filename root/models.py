5ba1aa82721ef3827e7823a563b1975f878ddaa6
yac72mh <mehrabi.yac@gmail.com>
2023-07-15 11:55:08 -0700

    make models

--- root/models.py
+++ root/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Services(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
